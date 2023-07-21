cca9c23455bd34d5d5ad9c9ff9c6ee6bf6b73f95
RahulJaincorpseed <rahul.jain@corpseed.com>
2023-07-21 17:19:28 +0530

    add some checks for one DImension  arrayRotation

--- RotateOneDArray.js
+++ RotateOneDArray.js
@@ -1,5 +1,5 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8]
-const rotation = 0
+const rotation =20
 const len = arr.length
 console.log(len)
 
@@ -7,12 +7,17 @@ const RotateArray = (array, len, rotate) => {
   if (rotate === 0) {
     return array
   }
+  if(len < rotate){
+    rotate = rotate%len;
+  } 
+  console.log("new rotate", rotate);
   for (let i = len - 1; i >= 0; i++) {
     if (rotate === 0) {
       return array
     }
     let elem = array.pop()
     array.unshift(elem)
+    console.log(`rotate ${i}`)
     rotate--
   }
 }
