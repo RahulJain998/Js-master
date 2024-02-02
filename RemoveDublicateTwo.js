3f62d6071b74049f227223196c03ede4de744d67
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-02-02 14:36:23 +0530

    add birdGame, CommonElement and other 4 programs

---
+++ RemoveDublicateTwo.js
@@ -0,0 +1,18 @@
+const array = [1, 1, 3, 3, 1, 3, 1, 1, 3, 4, 1, 3, 10]
+const newArray = []
+
+const removeDublicateTwo = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      console.log("j is", j, "i is ", i)
+      if (arr[i] === arr[j]) {
+        let variable = arr.splice(j, 1)
+      }
+      console.log("new array", arr)
+    }
+  }
+  return arr
+}
+
+console.log(removeDublicateTwo(array, newArray))
+  
